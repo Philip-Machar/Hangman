544ec2314b68306f10b60d6496c2361774117691
Philip-Machar <philipmachar788@gmail.com>
2024-11-29 14:02:30 +0300

    Initial commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,15 @@
+const Intro = () => {
+  return (
+    <div className="bg-[#261676] w-[324px] h-[481px] rounded-3xl flex justify-center relative opacity-90">
+      <div className="bg-[#2463FF] w-[315px] h-[470px] rounded-3xl flex justify-center absolute top-0 opacity-90">
+        <div className="bg-[#261676] w-[306px] h-[459px] rounded-3xl absolute bottom-0 opacity-60">
+          {/* Content here */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
+
+  
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Intro from "../components/Intro"
+
+const Homepage = () => {
+  return (
+    <div className="bg-[url(/assets/images/background-mobile.svg)] bg-cover bg-center h-screen grid place-content-center">
+      <Intro />
+    </div>
+  )
+}
+
+export default Homepage
