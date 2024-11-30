e96789a918864a444173c6e34a7caacb9f2ed69b
Philip-Machar <philipmachar788@gmail.com>
2024-11-30 11:56:55 +0300

    Complete homepage monile

--- src/components/HowToPlayButton.jsx
+++ src/components/HowToPlayButton.jsx
@@ -4,7 +4,7 @@ const HowToPlayButton = () => {
         <div className="text-white bg-black w-[260px] h-[62px] rounded-[42px] flex justify-center relative bg-opacity-30">
             <div className="bg-[#4b7dfc] w-[252px] h-[56px] rounded-[42px] absolute top-[1.5px] flex justify-center">
                 <div className="w-full h-full rounded-[42px] flex justify-center items-center relative">
-                    <div className="bg-[#2463FF] w-[244px] h-[50px] rounded-[42px] absolute bottom-0 grid place-content-center">HOW TO PLAY</div>
+                    <div className="bg-[#2463FF] w-[244px] h-[50px] rounded-[42px] absolute bottom-0 grid place-content-center text-3xl">HOW TO PLAY</div>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mouseMemoir: ["Mouse Memoirs", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
