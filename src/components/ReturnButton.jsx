94436fd029d526cb66e4241b47f5a7b721ff6dd5
Philip-Machar <philipmachar788@gmail.com>
2024-11-30 14:19:21 +0300

    Complete how to play page mobile

---
+++ src/components/ReturnButton.jsx
@@ -0,0 +1,11 @@
+const ReturnButton = () => {
+  return (
+    <div className="bg-gradient-to-b from-[#FE71FE] to-[#7199FF] w-[60px] h-[60px] rounded-full flex justify-center relative">
+        <div className="bg-gradient-to-b from-[#FE71FE] to-[#7199FF] w-[55px] h-[55px] rounded-full absolute top-0 flex justify-center">
+            <img src="/assets/images/icon-back.svg" width="30px" alt="back icon" />
+        </div>
+    </div>
+  )
+}
+
+export default ReturnButton
--- src/pages/HowToPlayPage.jsx
+++ src/pages/HowToPlayPage.jsx
@@ -1,21 +1,49 @@
+import ReturnButton from "../components/ReturnButton"
+
 const HowToPlayPage = () => {
   return (
     <div className="bg-[url(/assets/images/background-mobile.svg)] bg-cover bg-center h-screen grid place-content-center relative">
-      <div className="w-full absolute top-[16px] flex justify-between">
-        <div className="bg-gradient-to-b from-[#FE71FE] to-[#7199FF] w-[60px] h-[60px] rounded-full flex justify-center relative">
-            <div className="bg-gradient-to-b from-[#FE71FE] to-[#7199FF] w-[50px] h-[50px] rounded-full absolute top-0 flex justify-center">
-                <img src="/assets/images/icon-back.svg" width="30px" alt="back icon" />
-            </div>
-        </div>
+      <div className="w-full absolute top-8 flex justify-between px-8">
+        <ReturnButton />
         <img src="/assets/images/icon-how-to-play.png" alt="How to play heading" />
       </div>
-      <div>
-        {/* card
-        card
-        card */}
+      <div className="px-8 flex flex-col gap-12 mt-16 tracking-wide">
+        <div className="bg-white rounded-3xl w-[324px] h-[185px] grid place-content-center px-8">
+            <div className="flex text-2xl gap-4">
+                <div className="text-[#2463FF]">01</div>
+                <h1 className="text-[#261676]">CHOOSE A CATEGORY</h1>
+            </div>
+            <p className="text-[#887DC0] text-lg">
+                First, choose a word category, like animals or movies. 
+                The computer then randomly selects a secret word from that 
+                topic and shows you blanks for each letter of the word.
+            </p>
+        </div>
+        <div className="bg-white rounded-3xl w-[324px] h-[185px] grid place-content-center px-8">
+            <div className="flex text-2xl gap-4">
+                <div className="text-[#2463FF]">02</div>
+                <h1 className="text-[#261676]">GUESS LETTERS</h1>
+            </div>
+            <p className="text-[#887DC0] text-lg">
+                Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct. 
+                If it’s wrong, you lose some health, which empties after eight incorrect guesses.
+            </p>
+        </div>
+        <div className="bg-white rounded-3xl w-[324px] h-[185px] grid place-content-center px-8">
+            <div className="flex text-2xl gap-4">
+                <div className="text-[#2463FF]">03</div>
+                <h1 className="text-[#261676]">WIN OR LOSE</h1>
+            </div>
+            <p className="text-[#887DC0] text-lg">
+                You win by guessing all the letters in the word before your health runs out. 
+                If the health bar empties before you guess the word, you lose.
+            </p>
+        </div>
       </div>
     </div>
   )
 }
 
 export default HowToPlayPage
+
+
