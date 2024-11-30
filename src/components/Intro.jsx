66a62adf5292c3c85396ac4b00cbc1326cb63992
Philip-Machar <philipmachar788@gmail.com>
2024-11-30 11:13:11 +0300

    Complete homepage mobile

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,13 +1,14 @@
+import HowToPlayButton from "./HowToPlayButton";
 import PlayButton from "./PlayButton";
 
 const Intro = () => {
   return (
     <div className="bg-[#261676] w-[324px] h-[481px] rounded-[42px] flex justify-center relative opacity-90">
       <div className="bg-[#2463FF] w-[315px] h-[470px] rounded-[42px] flex justify-center absolute top-0 bg-opacity-90">
-        <div className="bg-[#261676] w-[306px] h-[459px] rounded-[42px] absolute bottom-0 bg-opacity-60 flex justify-center items-center">
+        <div className="bg-[#261676] w-[306px] h-[459px] rounded-[42px] absolute bottom-0 bg-opacity-60 flex flex-col justify-center items-center">
           <img src="/assets/images/logo.svg" width="250px" alt="hangman logo" className="absolute top-[-70px]" />
           <PlayButton />
-          <button className="text-white">HOW TO PLAY</button>
+          <HowToPlayButton />
         </div>
       </div>
     </div>
--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -1,11 +1,13 @@
 const PlayButton = () => {
   return (
-    <div className="bg-[#000000] w-[160px] h-[160px] rounded-full flex justify-center relative bg-opacity-30 cursor-pointer">
-      <div className="bg-[#FE71FE] w-[152px] h-[152px] rounded-full flex justify-center absolute top-0">
-        <div className="bg-gradient-to-b from-[#FE71FE] to-[#7199FF] w-[142px] h-[142px] rounded-full absolute top-0 grid place-content-center">
-            <img src="/assets/images/icon-play.svg" alt="play icon"/>
+    <div className="absolute top-[125px]">
+        <div className="bg-[#000000] w-[160px] h-[160px] rounded-full flex justify-center relative bg-opacity-30 cursor-pointer">
+            <div className="bg-[#FE71FE] w-[152px] h-[152px] rounded-full flex justify-center absolute top-0">
+                <div className="bg-gradient-to-b from-[#FE71FE] to-[#7199FF] w-[142px] h-[142px] rounded-full absolute top-0 grid place-content-center">
+                    <img src="/assets/images/icon-play.svg" alt="play icon"/>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   )
 }
