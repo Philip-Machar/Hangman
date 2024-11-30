655f4f78b282941b4edffb9eeed07fdfcca82b41
Philip-Machar <philipmachar788@gmail.com>
2024-11-30 20:39:17 +0300

    Complete category page mobile

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -8,24 +8,24 @@ const Categories = () => {
         <ReturnButton />
         <img src="/assets/images/pick-category-icon.png" alt="Pic caterogy icon" />
       </div>
-      <div className="flex flex-col gap-6">
-        <div className="bg-[#3d73fb] w-[324px] h-[77px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[320px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">MOVIES</div>
+      <div className="flex flex-col gap-6 mt-20">
+        <div className="bg-[#3d73fb] w-[330px] h-[77px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[326px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">MOVIES</div>
         </div>
-        <div className="bg-[#3d73fb] w-[324px] h-[77px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[320px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">TV SHOWS</div>
+        <div className="bg-[#3d73fb] w-[330px] h-[77px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[326px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">TV SHOWS</div>
         </div>
-        <div className="bg-[#3d73fb] w-[324px] h-[77px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[320px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">COUNTRIES</div>
+        <div className="bg-[#3d73fb] w-[330px] h-[77px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[326px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">COUNTRIES</div>
         </div>
-        <div className="bg-[#3d73fb] w-[324px] h-[77px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[320px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">CAPITAL CITIES</div>
+        <div className="bg-[#3d73fb] w-[330px] h-[77px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[326px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">CAPITAL CITIES</div>
         </div>
-        <div className="bg-[#3d73fb] w-[324px] h-[77px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[320px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">ANIMALS</div>
+        <div className="bg-[#3d73fb] w-[330px] h-[77px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[326px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">ANIMALS</div>
         </div>
-        <div className="bg-[#3d73fb] w-[324px] h-[77px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[320px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">SPORTS</div>
+        <div className="bg-[#3d73fb] w-[330px] h-[77px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[326px] h-[72px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl">SPORTS</div>
         </div>
       </div>
     </div>
