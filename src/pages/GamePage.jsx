82223a30e5e9d1f51e29fd5b295d61d028bea8e5
Philip-Machar <philipmachar788@gmail.com>
2024-12-01 13:11:16 +0300

    Complete game page

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,7 +1,7 @@
 const GamePage = () => {
   return (
     <div className="bg-[url(/assets/images/background-mobile.svg)] bg-cover bg-center h-screen relative">
-      <div class="absolute inset-0 bg-black/20"></div>
+      <div class="absolute inset-0 bg-black/50"></div>
       {/* Head */}
       <div className="absolute top-[40px] px-6 flex justify-between w-full">
         <div className="flex items-center gap-4">
@@ -64,8 +64,39 @@ const GamePage = () => {
         </div>
       </div>
       {/* Letters */}
-      <div>
-
+      <div className="px-6 mt-[130px] grid gap-4">
+        <div className="grid grid-cols-9 z-10">
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center opacity-30"></div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">B</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">C</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">D</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center opacity-30"></div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">F</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">G</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">H</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">I</div>
+        </div>
+        <div className="grid grid-cols-9 z-10">
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">J</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">K</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">L</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">M</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">N</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">O</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">P</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">Q</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center opacity-30"></div>
+        </div>
+        <div className="grid grid-cols-9 translate-x-6">
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">S</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center opacity-30"></div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">U</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">V</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">W</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">X</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">Y</div>
+            <div className="w-[29px] h-[56px] bg-white text-[#261676] text-[24px] rounded-lg grid place-content-center">Z</div>
+        </div>
       </div>
     </div>
   )
