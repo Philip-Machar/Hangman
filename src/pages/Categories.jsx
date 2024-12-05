0f186d7a1c6fd774abd23a57666553368dc18d3a
Philip-Machar <philipmachar788@gmail.com>
2024-12-05 10:41:54 +0300

    Complete desktop design

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -2,31 +2,31 @@ import ReturnButton from "../components/ReturnButton"
 
 const Categories = () => {
   return (
-    <div className="bg-[url(/assets/images/background-mobile.svg)] md:bg-[url(/assets/images/background-tablet.svg)] bg-cover bg-center h-screen grid place-content-center relative">
+    <div className="bg-[url(/assets/images/background-mobile.svg)] md:bg-[url(/assets/images/background-tablet.svg)] lg:bg-[url(/assets/images/background-desktop.svg)] bg-cover bg-center h-screen grid place-content-center relative">
       <div class="absolute inset-0 bg-black/50"></div>
-      <div className="w-full absolute top-[30px] md:top-[78px] flex justify-between px-6 md:px-12 lg:px-24">
+      <div className="w-full absolute top-[30px] md:top-[78px] lg:top-[50px] flex justify-between px-6 md:px-12 lg:px-24">
         <ReturnButton />
         <img src="/assets/images/pick-category-icon.png" alt="Pic caterogy icon" className="md:w-[343px]" />
         <div className="hidden md:block"></div>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[270px] h-[60px] md:w-[250px] md:h-[150px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] tracking-wider">MOVIES</div>
+        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] lg:w-[208px] lg:h-[108px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[270px] h-[60px] md:w-[250px] md:h-[150px] lg:w-[200px] lg:h-[100px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] lg:text-[28px] tracking-wider">MOVIES</div>
         </div>
-        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] tracking-wider">TV SHOWS</div>
+        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] lg:w-[208px] lg:h-[108px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] lg:w-[200px] lg:h-[100px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] lg:text-[28px] tracking-wider">TV SHOWS</div>
         </div>
-        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] tracking-wider">COUNTRIES</div>
+        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] lg:w-[208px] lg:h-[108px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] lg:w-[200px] lg:h-[100px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] lg:text-[28px] tracking-wider">COUNTRIES</div>
         </div>
-        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] tracking-wider">CAPITAL CITIES</div>
+        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] lg:w-[208px] lg:h-[108px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] lg:w-[200px] lg:h-[100px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] lg:text-[28px] tracking-wider">CAPITAL CITIES</div>
         </div>
-        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] tracking-wider">ANIMALS</div>
+        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] lg:w-[208px] lg:h-[108px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] lg:w-[200px] lg:h-[100px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] lg:text-[28px] tracking-wider">ANIMALS</div>
         </div>
-        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] relative flex justify-center rounded-3xl">
-            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] tracking-wider">SPORTS</div>
+        <div className="bg-[#3d73fb] w-[276px] h-[65px] md:w-[260px] md:h-[160px] lg:w-[208px] lg:h-[108px] relative flex justify-center rounded-3xl">
+            <div className="bg-[#2463FF] w-[276px] h-[65px] md:w-[250px] md:h-[150px] lg:w-[200px] lg:h-[100px] absolute bottom-0 rounded-3xl grid place-content-center text-white text-2xl md:text-[36px] lg:text-[28px] tracking-wider">SPORTS</div>
         </div>
       </div>
     </div>
--- src/pages/HowToPlayPage.jsx
+++ src/pages/HowToPlayPage.jsx
@@ -18,16 +18,13 @@ const HowToPlayPage = () => {
 
   return (
     <div
-      className="relative grid place-content-center"
+      className="bg-[url(/assets/images/background-mobile.svg)] md:bg-[url(/assets/images/background-tablet.svg)] lg:bg-[url(/assets/images/background-desktop.svg)] bg-cover bg-center relative grid place-content-center"
       style={{
         height: "calc(var(--vh, 1vh) * 100)",
-        backgroundImage: "url('/assets/images/background-mobile.svg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
       }}
     >
       <div className="absolute inset-0 bg-black/50"></div>
-      <div className="w-full absolute top-[30px] md:top-[78px] flex justify-between px-6 md:px-12 lg:px-24 lg:mb-24">
+      <div className="w-full absolute top-[30px] md:top-[78px] lg:top-[50px] flex justify-between px-6 md:px-12 lg:px-24 lg:mb-24">
         <ReturnButton />
         <img
           src="/assets/images/icon-how-to-play.png"
@@ -37,10 +34,10 @@ const HowToPlayPage = () => {
         <div className="hidden md:block"></div>
       </div>
       <div className="px-6 md:px-24 lg:px-24 flex flex-col lg:flex-row gap-6 mt-20 tracking-wider">
-        <div className="bg-white rounded-3xl p-6 md:p-10 grid place-content-center gap-2 z-10">
-          <div className="flex text-2xl gap-4 lg:mb-4">
-            <div className="text-[#2463FF] md:text-[40px]">01</div>
-            <h1 className="text-[#261676] md:text-[40px]">CHOOSE A CATEGORY</h1>
+        <div className="bg-white rounded-3xl p-6 md:p-10 lg:p-6 grid place-content-center gap-2 z-10">
+          <div className="flex text-2xl gap-4">
+            <div className="text-[#2463FF] md:text-[40px] lg:text-[28px]">01</div>
+            <h1 className="text-[#261676] md:text-[40px] lg:text-[28px]">CHOOSE A CATEGORY</h1>
           </div>
           <p className="text-[#887DC0] text-[16px] md:text-[20px]">
             First, choose a word category, like animals or movies. The computer
@@ -48,10 +45,10 @@ const HowToPlayPage = () => {
             blanks for each letter of the word.
           </p>
         </div>
-        <div className="bg-white rounded-3xl p-6 md:p-10 grid place-content-center gap-2 px-8 z-10">
-          <div className="flex text-2xl gap-4 lg:mb-4">
-            <div className="text-[#2463FF] md:text-[40px]">02</div>
-            <h1 className="text-[#261676] md:text-[40px]">GUESS LETTERS</h1>
+        <div className="bg-white rounded-3xl p-6 md:p-10 lg:p-6 grid place-content-center gap-2 px-8 z-10">
+          <div className="flex text-2xl gap-4">
+            <div className="text-[#2463FF] md:text-[40px] lg:text-[28px]">02</div>
+            <h1 className="text-[#261676] md:text-[40px] lg:text-[28px]">GUESS LETTERS</h1>
           </div>
           <p className="text-[#887DC0] md:text-[20px]">
             Take turns guessing letters. The computer fills in the relevant blank
@@ -59,10 +56,10 @@ const HowToPlayPage = () => {
             which empties after eight incorrect guesses.
           </p>
         </div>
-        <div className="bg-white rounded-3xl p-6 md:p-10 grid place-content-center gap-2 px-8 z-10">
-          <div className="flex text-2xl gap-4 lg:mb-4">
-            <div className="text-[#2463FF] md:text-[40px]">03</div>
-            <h1 className="text-[#261676] md:text-[40px]">WIN OR LOSE</h1>
+        <div className="bg-white rounded-3xl p-6 md:p-10 lg:p-6 grid place-content-center gap-2 px-8 z-10">
+          <div className="flex text-2xl gap-4">
+            <div className="text-[#2463FF] md:text-[40px] lg:text-[28px]">03</div>
+            <h1 className="text-[#261676] md:text-[40px] lg:text-[28px]">WIN OR LOSE</h1>
           </div>
           <p className="text-[#887DC0] md:text-[20px]">
             You win by guessing all the letters in the word before your health
