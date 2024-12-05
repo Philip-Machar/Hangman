3593bd60b872a5aaac505fffa80d50afe74b31b3
Philip-Machar <philipmachar788@gmail.com>
2024-12-05 06:45:35 +0300

    Complete mobile debugging

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import PlayButton from "./PlayButton";
 
 const Intro = () => {
   return (
-    <div className="bg-[#261676] sm:w-[90%] sm:h-auto w-[240px] h-[340px] md:w-[592px] md:h-[500px] rounded-[42px] flex justify-center relative opacity-90">
+    <div className="bg-[#261676] w-[240px] h-[340px] md:w-[592px] md:h-[500px] rounded-[42px] flex justify-center relative opacity-90">
       <div className="bg-[#2463FF] w-[232px] h-[332px] md:w-[575px] md:h-[485px] rounded-[42px] flex justify-center absolute top-0 bg-opacity-90">
         <div className="bg-[#261676] w-[223px] h-[325px] md:w-[560px] md:h-[470px] rounded-[42px] absolute bottom-0 bg-opacity-60 flex flex-col justify-center items-center">
           <img src="/assets/images/logo.svg" alt="hangman logo" className="w-[150px] md:w-[356px] absolute top-[-40px] md:top-[-100px]" />
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -5,10 +5,10 @@ import YouWin from "../components/YouWin"
 const GamePage = () => {
   return (
     <div className="bg-[url(/assets/images/background-mobile.svg)] md:bg-[url(/assets/images/background-tablet.svg)] lg:bg-[url(/assets/images/background-desktop.svg)] bg-cover bg-center h-screen relative flex flex-col items-center">
-      <div class="absolute inset-0 bg-black/50"></div>
-      {/* <div class="absolute inset-0 bg-black/60 z-20"></div> */}
+      {/* <div class="absolute inset-0 bg-black/50"></div> */}
+      <div class="absolute inset-0 bg-black/60 z-20"></div>
       {/* <Paused /> */}
-      {/* <YouWin /> */}
+      <YouWin />
       {/* <YouLose /> */}
       {/* Head */}
       <div className="absolute top-[30px] md:top-[78px] px-6 md:px-12 flex justify-between w-full">
@@ -47,29 +47,6 @@ const GamePage = () => {
                 <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#2463FF] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0">D</div>
             </div>
         </div>
-        <div className="grid grid-cols-9 gap-3">
-            <div className="w-[29px] h-[45px] md:w-[88px] md:h-[112px] bg-[#2f1e83] flex justify-center rounded-xl md:rounded-[30px] relative">
-                <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#261676] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0"></div>
-            </div>
-            <div className="w-[29px] h-[45px] md:w-[88px] md:h-[112px] bg-[#3d73fb] flex justify-center rounded-xl md:rounded-[30px] relative">
-                <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#2463FF] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0">M</div>
-            </div>
-            <div className="w-[29px] h-[45px] md:w-[88px] md:h-[112px] bg-[#3d73fb] flex justify-center rounded-xl md:rounded-[30px] relative">
-                <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#2463FF] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0">E</div>
-            </div>
-            <div className="w-[29px] h-[45px] md:w-[88px] md:h-[112px] bg-[#2f1e83] flex justify-center rounded-xl md:rounded-[30px] relative">
-                <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#261676] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0"></div>
-            </div>
-            <div className="w-[29px] h-[45px] md:w-[88px] md:h-[112px] bg-[#3d73fb] flex justify-center rounded-xl md:rounded-[30px] relative">
-                <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#2463FF] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0">I</div>
-            </div>
-            <div className="w-[29px] h-[45px] md:w-[88px] md:h-[112px] bg-[#3d73fb] flex justify-center rounded-xl md:rounded-[30px] relative">
-                <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#2463FF] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0">C</div>
-            </div>
-            <div className="w-[29px] h-[45px] md:w-[88px] md:h-[112px] bg-[#2f1e83] flex justify-center rounded-xl md:rounded-[30px] relative">
-                <div className="w-[25px] h-[41px] md:w-[78px] md:h-[105px] bg-[#261676] rounded-xl md:rounded-[30px] text-white grid place-content-center text-[28px] md:text-[64px] absolute bottom-0"></div>
-            </div>
-        </div>
       </div>
       {/* Letters */}
       <div className="w-full px-6 md:px-12 mt-[130px] grid gap-6">
@@ -95,7 +72,7 @@ const GamePage = () => {
             <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] bg-white text-[#261676] text-[24px] md:text-[42px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">Q</div>
             <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] bg-white text-[#261676] text-[24px] md:text-[42px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer opacity-40">R</div>
         </div>
-        <div className="grid grid-cols-9 translate-x-6 md:translate-x-9 z-10">
+        <div className="grid grid-cols-9 translate-x-4 md:translate-x-9 z-10">
             <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] bg-white text-[#261676] text-[24px] md:text-[42px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">S</div>
             <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] bg-white text-[#261676] text-[24px] md:text-[42px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer opacity-40">T</div>
             <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] bg-white text-[#261676] text-[24px] md:text-[42px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">U</div>
