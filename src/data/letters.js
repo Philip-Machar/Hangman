c27ddb4022890ef21568fd5beb1d39c70084056d
Philip-Machar <philipmachar788@gmail.com>
2024-12-07 19:37:14 +0300

    Complete dynamic letter render

---
+++ src/data/letters.js
@@ -0,0 +1,32 @@
+// src/data/letters.js
+const letters = [
+    { id: 1, letter: "A", opacity: true },
+    { id: 2, letter: "B", opacity: false },
+    { id: 3, letter: "C", opacity: false },
+    { id: 4, letter: "D", opacity: false },
+    { id: 5, letter: "E", opacity: true },
+    { id: 6, letter: "F", opacity: false },
+    { id: 7, letter: "G", opacity: false },
+    { id: 8, letter: "H", opacity: false },
+    { id: 9, letter: "I", opacity: false },
+    { id: 10, letter: "J", opacity: false },
+    { id: 11, letter: "K", opacity: false },
+    { id: 12, letter: "L", opacity: false },
+    { id: 13, letter: "M", opacity: false },
+    { id: 14, letter: "N", opacity: false },
+    { id: 15, letter: "O", opacity: false },
+    { id: 16, letter: "P", opacity: false },
+    { id: 17, letter: "Q", opacity: false },
+    { id: 18, letter: "R", opacity: true },
+    { id: 19, letter: "S", opacity: false },
+    { id: 20, letter: "T", opacity: true },
+    { id: 21, letter: "U", opacity: false },
+    { id: 22, letter: "V", opacity: false },
+    { id: 23, letter: "W", opacity: false },
+    { id: 24, letter: "X", opacity: false },
+    { id: 25, letter: "Y", opacity: false },
+    { id: 26, letter: "Z", opacity: false },
+  ];
+  
+  export default letters;
+  
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import Paused from "../components/Paused";
 import YouLose from "../components/YouLose";
 import YouWin from "../components/YouWin";
+import letters from "../data/letters";
 
 const GamePage = ({ category }) => {
   const [isPaused, setIsPaused] = useState(false);
-  const [life, setLife] = useState(100);
+  const [health, setHealth] = useState(100);
   const [movie, setMovie] = useState([]);
   
   //pauses the game
@@ -17,7 +18,7 @@ const GamePage = ({ category }) => {
  useEffect(() => {
     if (category === "MOVIES"){
         const getMovie = async () => {
-            const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
             const randomPageIndex = Math.floor(Math.random() * pages.length);
             const page = pages[randomPageIndex];
             try {
@@ -64,7 +65,7 @@ const GamePage = ({ category }) => {
         </div>
         <div className="flex justify-center items-center gap-4 md:gap-8">
             <div className="w-[57px] h-[16px] md:w-[160px] md:h-[31px] bg-white rounded-3xl flex items-center">
-                <div style={{ width: `${life}%`}} className="bg-[#261676] h-[8px] md:w-[40%] md:h-[13px] rounded-3xl mx-1 md:mx-3"></div>
+                <div style={{ width: `${health}%`}} className="bg-[#261676] h-[8px] md:w-[40%] md:h-[13px] rounded-3xl mx-1 md:mx-3"></div>
             </div>
             <img src="/assets/images/icon-heart.svg" alt="Heart icon" className="w-[26px] md:w-[53px]"/>
         </div>
@@ -76,7 +77,7 @@ const GamePage = ({ category }) => {
                 movie.map((letter, index) => {
                     return (
                         <div key={index} className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-[#2f1e83] flex justify-center rounded-xl md:rounded-[24px] lg:rounded-[18px] relative">
-                            <div className="w-[25px] h-[41px] md:w-[58px] md:h-[78px] lg:w-[45px] lg:h-[62px] bg-[#261676] rounded-xl md:rounded-[24px] lg:rounded-[18px] text-white grid place-content-center text-[28px] md:text-[48px] lg:text-[36px] absolute bottom-0">{letter}</div>
+                            <div className="w-[25px] h-[41px] md:w-[58px] md:h-[78px] lg:w-[45px] lg:h-[62px] bg-[#261676] rounded-xl md:rounded-[24px] lg:rounded-[18px] text-white grid place-content-center text-[28px] md:text-[48px] lg:text-[36px] absolute bottom-0"></div>
                         </div>
                     )
                 })
@@ -103,38 +104,17 @@ const GamePage = ({ category }) => {
       </div>
       {/* Letters */}
       <div className="w-full md:w-auto px-6 md:px-12 mt-[120px] lg:mt-[80px] grid gap-4 justify-center">
-        <div className="grid grid-cols-9 gap-2 md:gap-4 z-10 justify-center">
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer opacity-40">A</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">B</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">C</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">D</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer opacity-40">E</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">F</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">G</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">H</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">I</div>
-        </div>
-        <div className="grid grid-cols-9 gap-2 md:gap-4 z-10 justify-center">
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">J</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">K</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">L</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">M</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">N</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">O</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">P</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">Q</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer opacity-40">R</div>
-        </div>
-        <div className="grid grid-cols-9 gap-2 md:gap-4 z-10 justify-center translate-x-4 md:translate-x-9">
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">S</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer opacity-40">T</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">U</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">V</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">W</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">X</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">Y</div>
-            <div className="w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer">Z</div>
-        </div>
+        {/* Dynamically create rows */}
+        {[...Array(3)].map((_, rowIndex) => (
+            <div key={rowIndex} className={`grid grid-cols-9 gap-2 md:gap-4 z-10 justify-center ${rowIndex === 2 ? "translate-x-4 md:translate-x-9" : ""}`}>
+                {/* Dynamically populate letters */}
+                {
+                    letters.slice(rowIndex * 9, rowIndex * 9 + 9).map(({ id, letter, opacity }) => (
+                        <div key={id} className={`w-[29px] h-[45px] md:w-[64px] md:h-[84px] lg:w-[52px] lg:h-[67px] bg-white text-[#261676] text-[24px] md:text-[42px] lg:text-[36px] rounded-lg md:rounded-[20px] grid place-content-center cursor-pointer ${opacity ? "opacity-40" : ""}`}>{letter}</div>
+                    ))
+                }
+            </div>
+        ))}
       </div>
     </div>
   );
