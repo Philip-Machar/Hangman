3036028495e56201ffee151e3b2b86e9a227c882
Philip-Machar <philipmachar788@gmail.com>
2024-12-10 22:23:11 +0300

    Complete game logic

--- src/components/Paused.jsx
+++ src/components/Paused.jsx
@@ -8,9 +8,9 @@ const Paused = ({ setIsPaused, handleHomepage }) => {
 
   return (
     <div className="absolute top-[200px] md:top-[300px] lg:top-[150px]">
-        <div className="bg-[#261676] w-[240px] h-[340px] md:w-[470px] md:h-[494px] lg:w-[370px] lg:h-[420px] rounded-[42px] md:rounded-[70px] flex justify-center relative opacity-90 z-20">
-            <div className="bg-[#2463FF] w-[232px] h-[332px] md:w-[460px] md:h-[482px] lg:w-[360px] lg:h-[410px] rounded-[42px] md:rounded-[70px] flex justify-center absolute top-0 bg-opacity-90">
-                <div className="bg-[#261676] w-[223px] h-[325px] md:w-[450px] md:h-[470px] lg:w-[350px] lg:h-[400px] rounded-[42px] md:rounded-[70px] absolute bottom-0 bg-opacity-60 flex flex-col justify-center items-center gap-8">
+        <div className="bg-[#261676] w-[240px] h-[280px] md:w-[470px] md:h-[494px] lg:w-[370px] lg:h-[420px] rounded-[42px] md:rounded-[70px] flex justify-center relative opacity-90 z-20">
+            <div className="bg-[#2463FF] w-[232px] h-[272px] md:w-[460px] md:h-[482px] lg:w-[360px] lg:h-[410px] rounded-[42px] md:rounded-[70px] flex justify-center absolute top-0 bg-opacity-90">
+                <div className="bg-[#261676] w-[223px] h-[265px] md:w-[450px] md:h-[470px] lg:w-[350px] lg:h-[400px] rounded-[42px] md:rounded-[70px] absolute bottom-0 bg-opacity-60 flex flex-col justify-center items-center gap-8">
                     <img src="/assets/images/Paused.png" alt="hangman logo" className="absolute w-[150px] md:w-[265px] lg:w-[220px] top-[-40px] md:top-[-70px] lg:top-[-65px]" />
                     {/* button 1*/}
                     <div onClick={handleIsPaused} className="text-white bg-black w-[195px] h-[56px] md:w-[320px] md:h-[80px] lg:w-[264px] lg:h-[78px] rounded-[42px] flex justify-center relative bg-opacity-30 cursor-pointer">
