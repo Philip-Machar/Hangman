f9679d25f507e2a26fecc9ddabbbb0481bbde963
Philip-Machar <philipmachar788@gmail.com>
2024-12-11 11:58:48 +0300

    Complete game logic

--- src/components/YouLose.jsx
+++ src/components/YouLose.jsx
@@ -28,11 +28,11 @@ const YouLose = ({ word, category, handlePlayAgain, handleHomepage }) => {
                             </div>
                         </div>
                     </div>
-                    {/* button 1*/}
-                    <div onClick={handlePlayAgain} className="text-white bg-black w-[195px] h-[56px] md:w-[320px] md:h-[80px] lg:w-[264px] lg:h-[78px] rounded-[42px] flex justify-center relative bg-opacity-30">
-                        <div className="bg-[#3d73fb] w-[188px] h-[50px] md:w-[310px] md:h-[70px] lg:w-[258px] rounded-[42px] absolute top-[1.5px] flex justify-center">
-                            <div className="w-full h-full rounded-[42px] flex justify-center items-center relative">
-                                <div className="bg-[#2463FF] w-[180px] h-[45px] md:w-[300px] md:h-[62px] lg:w-[252px] lg:h-[64px] rounded-[42px] absolute bottom-0 grid place-content-center text-2xl md:text-3xl lg:text-2xl tracking-wider">PLAY AGAIN</div>
+                    {/* button 2*/}
+                    <div onClick={handlePlayAgain} className="text-white bg-black w-[195px] h-[56px] md:w-[320px] md:h-[80px] lg:w-[264px] lg:h-[78px] rounded-[42px] flex justify-center relative bg-opacity-30 cursor-pointer">
+                        <div className="bg-[#3d73fb] w-[188px] h-[50px] md:w-[310px] md:h-[70px] lg:w-[258px] rounded-[42px] absolute top-[1.5px] flex justify-center pointer-events-none">
+                            <div className="w-full h-full rounded-[42px] flex justify-center items-center relative pointer-events-none">
+                                <div className="bg-[#2463FF] w-[180px] h-[45px] md:w-[300px] md:h-[62px] lg:w-[252px] lg:h-[64px] rounded-[42px] absolute bottom-0 grid place-content-center text-2xl md:text-3xl lg:text-2xl tracking-wider pointer-events-none">PLAY AGAIN</div>
                             </div>
                         </div>
                     </div>
--- src/components/YouWin.jsx
+++ src/components/YouWin.jsx
@@ -8,10 +8,10 @@ const YouWin = ({ handlePlayAgain, handleHomepage }) => {
                 <div className="bg-[#261676] w-[223px] h-[265px] md:w-[450px] md:h-[400px] lg:w-[350px] lg:h-[340px] rounded-[42px] md:rounded-[70px] absolute bottom-0 bg-opacity-60 flex flex-col justify-center items-center gap-8">
                     <img src="/assets/images/you-win-logo.png" alt="hangman logo" className="absolute w-[150px] md:w-[265px] lg:w-[220px] top-[-40px] md:top-[-65px] lg:top-[-56px]" />
                     {/* button 1*/}
-                    <div onClick={handlePlayAgain} className="text-white bg-black w-[195px] h-[56px] md:w-[320px] md:h-[80px] lg:w-[264px] lg:h-[78px] rounded-[42px] flex justify-center relative bg-opacity-30">
-                        <div className="bg-[#3d73fb] w-[188px] h-[50px] md:w-[310px] md:h-[70px] lg:w-[258px] rounded-[42px] absolute top-[1.5px] flex justify-center">
-                            <div className="w-full h-full rounded-[42px] flex justify-center items-center relative">
-                                <div className="bg-[#2463FF] w-[180px] h-[45px] md:w-[300px] md:h-[62px] lg:w-[252px] lg:h-[64px] rounded-[42px] absolute bottom-0 grid place-content-center text-2xl md:text-3xl lg:text-2xl tracking-wider">PLAY AGAIN</div>
+                    <div onClick={handlePlayAgain} className="text-white bg-black w-[195px] h-[56px] md:w-[320px] md:h-[80px] lg:w-[264px] lg:h-[78px] rounded-[42px] flex justify-center relative bg-opacity-30 cursor-pointer">
+                        <div className="bg-[#3d73fb] w-[188px] h-[50px] md:w-[310px] md:h-[70px] lg:w-[258px] rounded-[42px] absolute top-[1.5px] flex justify-center pointer-events-none">
+                            <div className="w-full h-full rounded-[42px] flex justify-center items-center relative pointer-events-none">
+                                <div className="bg-[#2463FF] w-[180px] h-[45px] md:w-[300px] md:h-[62px] lg:w-[252px] lg:h-[64px] rounded-[42px] absolute bottom-0 grid place-content-center text-2xl md:text-3xl lg:text-2xl tracking-wider pointer-events-none">PLAY AGAIN</div>
                             </div>
                         </div>
                     </div>
